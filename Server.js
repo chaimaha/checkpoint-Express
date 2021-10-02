d193b5ed5b3aeba268b0d74dbe7eba61e92658eb
rezgui chaima <rezguichaima2911@gmail.com>
2021-10-03 01:55:52 +0200

    to be completed

---
+++ Server.js
@@ -0,0 +1,71 @@
+//clear Console
+// console.clear();
+//import
+import express from "express";
+import path from "path";
+import IsAuth from "./middleware/auth";
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+//instance
+const app = express();
+//read JSON
+app.use(express.json());
+//TIMER-----------------
+app.use(IsAuth());
+//----------------------------------------------------------
+//template
+app.set("view engine", "pug");
+app.get("/service", function (req, res) {
+  res.render("Service", { posts });
+});
+
+// ----------------------------------------------
+
+const posts = [
+  { name: "post1", id: 1 },
+  { name: "post2", id: Math.random() },
+  { name: "post3", id: Math.random() },
+  { name: "post4", id: Math.random() },
+];
+app.get("/posts", (req, res) => {
+  res.send({ data: posts, msg: "success" });
+});
+app.get("/posts/:id", (req, res) => {
+  posts.map((ele) => ele.id == req.params.id)
+    ? res.send({ msg: "post finded", ele })
+    : res.send("not found");
+});
+app.post("/addPosts", (req, res) => {
+  let data = req.body;
+  posts.push({ ...data, id: Math.random() });
+  res.send({ msg: "posr added", posts });
+});
+app.put("/update/:id", (req, res) => {
+  const id = req.params.id;
+  let data = req.body;
+  posts.map((ele) => (ele.id == id ? { ...ele, ...data } : ele))
+    ? res.send({ msg: "post update", posts })
+    : res.send("error");
+});
+app.delete("/delete/:id", (req, res) => {
+  // parametre
+  let id = req.params.id;
+  posts = posts.filter((el) => el.id != id);
+  res.send({ msg: "post deleted", posts });
+});
+//-----------------------------
+app.get("/", (req, res) => {
+  res.sendFile(__dirname.slice(3) + "/Public/Home/Home.html");
+});
+// app.get("/service", (req, res) => {
+//   res.sendFile(__dirname.slice(3) + "/Public/OurService/Service.html");
+// });
+app.get("/contact", (req, res) =>
+  res.sendFile(__dirname.slice(3) + "/Public/ContactUs/Contact.html")
+);
+//port
+const PORT = 5000;
+//Server
+app.listen(PORT, (err) =>
+  err ? console.log(err) : console.log(`Server is running on ${PORT}`)
+);
---
+++ middleware/auth.js
@@ -0,0 +1,42 @@
+// a middleware function takes 3 parameters
+// req + res +next
+// si la condition est vrai
+// next to the callback function
+// else the middleware send the the response
+let current = new Date();
+
+let cTime =
+  current.getHours() + ":" + current.getMinutes() + ":" + current.getSeconds();
+let dateTime = cTime;
+console.log(dateTime);
+var d = new Date();
+var weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+var n = weekday[d.getDay()];
+console.log(n);
+const testDate = () => {
+  if (n !== "Saturday" && n !== "Sunday" && 9 < current.getHours() < 17)
+    return true;
+  else console.log("false");
+  return false;
+};
+// const auth = () => {
+//   if (testDate === true) console.log("ok");
+//   else console.log("sorry");
+// };
+
+const IsAuth = (req, res, next) => {
+  if (testDate() === true) {
+    next();
+  } else {
+    res.status(401).send("not authorised");
+  }
+};
+export default IsAuth;
